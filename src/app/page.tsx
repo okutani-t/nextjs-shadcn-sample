839759610b900aab9dfbc1154dca3d1d278342d8
okutani-t <okutani.nt@gmail.com>
2025-09-15 02:14:13 +0900

    Add shadcn sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,70 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Loader2Icon } from "lucide-react"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Home() {
   return (
-    <div className="container mx-auto pt-20">
-      <p>hello shadcn/ui!</p>
-    </div>
+    <main className="min-h-screen">
+      <div className="container mx-auto mt-20">
+        <h1 className="text-3xl font-bold mb-10 items-start">Next.js + shadcn/ui Sample</h1>
+        <div className="flex flex-col gap-10 items-start">
+          <Button >Click me!</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button size="sm" disabled>
+            <Loader2Icon className="animate-spin" />
+            Please wait
+          </Button>
+        </div>
+
+        <div className="max-w-2xl">
+          <Table className="mt-20">
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </main>
   )
 }
